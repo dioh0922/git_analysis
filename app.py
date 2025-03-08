082a15fc73ec7983de3ad9d19cea02554255dce7
dioh0922 <diohound@gmail.com>
2025-03-08 10:40:31 +0900

    gitapiの追加

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from flask import Flask, render_template, request, url_for, redirect, jsonify
 import magic
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 mime = magic.Magic(mime=True)
@@ -17,6 +21,24 @@ def method_not_allowed(error):
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/git')
+def git_analysis():
+  data = {
+    'query': 'query{user(login:"' + os.getenv('GIT_USER') + '"){contributionsCollection {contributionCalendar {weeks {contributionDays {contributionCount\ndate}}}}}}'
+  }
+  headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer  ' +  os.getenv('GIT_TOKEN'),
+    'User-Agent': 'Awesome-Octocat-App'
+  }
+
+  response = requests.post('https://api.github.com/graphql', json = data, headers = headers)
+
+  if response.status_code == 200:
+    return jsonify(response.json()), 200
+  else:
+    return [], 200
 
 @app.route('/request', methods=['POST'])
 def api():
